83e0cd992b3421a07ac8a9ec719752984ccecf90
binmaziabdullah <80192810+binmaziabdullah@users.noreply.github.com>
2021-05-08 04:07:03 -0800

    updated comments

--- src/main/java/com/projection/projectioninspringdatajpa/closedprojection/StudentRepository.java
+++ src/main/java/com/projection/projectioninspringdatajpa/closedprojection/StudentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
     /*Here we are getting the specific recording from student projection
-    * and setting the return type student projection interface*/
+    * and setting the return type student projection (interface)*/
     StudentProjection findByFirstName(String firstName);
 
     StudentProjection findByLastName(String lastName);
